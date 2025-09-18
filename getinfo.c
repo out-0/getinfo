4eac92e887b041b543dde69d5835bc74c53b546e
exe <ayoubarid000@gmail.com>
2025-09-18 16:46:53 +0100

    Modify to handle resizing window problem
    Separate the main function, to get a independent
    draw function that draw the menu into stdscreen
    so when the program detect a signal of resizing
    its its call the handle function that is read the
    new window sizes and draw the menu correspondent
    to those sizes, if now signal detected it draw
    the menu normally with the current sizes.

--- getinfo.c
+++ getinfo.c
@@ -1,63 +1,68 @@
 #include "getinfo.h"
 
+// Function declaration
+void	ft_draw_menu();
+
 int	main(void)
 {
 	t_windows	win;
-	int		row, col;
-	int		index, option;
+       	int		option;
+
+	initscr();               // Start curses mode
+	noecho();                // Disable input echo
+	cbreak();                // Disable line buffering
+	start_color();           // Enable colors
+	use_default_colors();    // Use default terminal background
+	init_pair(1, COLOR_YELLOW, -1);
+	init_pair(2, COLOR_CYAN, -1);
+
+	signal(SIGWINCH, ft_handle_window_change); // Handle resize
+
+	ft_draw_menu(); // Initial draw
+
+	// Main loop
+	while ((option = getch()) != '0')
+	{
+		if (option == '1')
+			ft_42program(&win);
+		// Add other options as needed
+	}
+	
+	endwin(); // End curses mode
+	return 0;
+}
+
+// Draw the main menu to the standard screen
+void	ft_draw_menu(void)
+{
+	int			row, col, index = 0;
 	const char	*title[] = {
-	"  ____ _____ _____   ___ _   _ _____ ___  ",
-	" / ___| ____|_   _| |_ _| \\ | |  ___/ _ \\ ",
-	"| |  _|  _|   | |    | ||  \\| | |_ | | | |",
-	"| |_| | |___  | |    | || |\\  |  _|| |_| |",
-	" \\____|_____| |_|   |___|_| \\_|_|   \\___/ ",
-	NULL
+		"  ____ _____ _____   ___ _   _ _____ ___  ",
+		" / ___| ____|_   _| |_ _| \\ | |  ___/ _ \\ ",
+		"| |  _|  _|   | |    | ||  \\| | |_ | | | |",
+		"| |_| | |___  | |    | || |\\  |  _|| |_| |",
+		" \\____|_____| |_|   |___|_| \\_|_|   \\___/ ",
+		NULL
 	};
 
-	initscr();	// Start the curses mode
-	noecho();	// Disblay the input characters
-	cbreak();	// Disable key buffering and keep key signals
-	start_color();	// Enable the color
-	index = 0;
-
-	use_default_colors();	// Set the background to default.
-	init_pair(1, COLOR_YELLOW, -1); // Inisial forground and let the default background
-	init_pair(2, COLOR_CYAN, -1);
-//	box(stdscr, 0,0);	//box
 	getmaxyx(stdscr, row, col);
-	
-	
 	row = 1;
-	// enable the attribute
+
 	attron(COLOR_PAIR(1) | A_BOLD);
-	// Print the ascii art title
 	while (title[index] != NULL)
 	{
 		mvprintw(row + index, (col - strlen(title[index])) / 2, "%s", title[index]);
 		index++;
 	}
 	attroff(COLOR_PAIR(1) | A_BOLD);
-	
+
 	getmaxyx(stdscr, row, col);
 	attron(A_BOLD | COLOR_PAIR(2));
-	// Sections
 	mvprintw((row * 0.5) + 4, 2, "[1] - 42Program");
 	mvprintw((row * 0.5) + 6, 2, "[2] - Web Development");
-	mvprintw((row * 0.5) + 8, 2, "[3] - Grphical Programming");
+	mvprintw((row * 0.5) + 8, 2, "[3] - Graphical Programming");
 	mvprintw(row - 2, col - 12, "[0] - EXIT");
 	attroff(A_BOLD | COLOR_PAIR(2));
-	
-	while ((option = getch()) != '0')
-	{
-		if (option == '1')
-		{
-			ft_42program(&win);
-		}
-	}
 
-	werase(stdscr);
-	refresh();	
-	endwin();
-
-	return (0);
+	refresh();
 }
--- getinfo.h
+++ getinfo.h
@@ -4,6 +4,8 @@
 #include <ncurses.h>
 #include <stdlib.h> // For system()
 #include <string.h> // For strlen()
+#include <signal.h> // For read signals recived from OS
+
 // Combine the windows in one struct for simple access
 typedef struct s_windows
 {
@@ -12,10 +14,13 @@ typedef struct s_windows
 	
 }	t_windows;
 
+void	ft_draw_menu(void);
 void	ft_42program(t_windows *win);
 void	ft_libft(t_windows *win);
-void	ft_opening_message(WINDOW *previous_window);	// Print link opening message
+void	ft_porn2beroot(t_windows *win);
 
-void	ft_open_link(char *url);
+void	ft_handle_window_change(int signal);	// Handle resizing
+void	ft_opening_message(WINDOW *previous_window);	// Print link opening message
+void	ft_open_link(char *url);	// Opne a link in the browser
 
 #endif
